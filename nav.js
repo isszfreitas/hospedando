e360e767e266fe2fd7ff30577d01ca34022f35be
Ismael Serejo de Freitas <ismaelbr13@gmail.com>
2024-06-20 14:41:24 -0300

    Update v1.2

---
+++ nav.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", function(){
+    let nav = this.document.querySelector ('#header')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+window.addEventListener("scroll", function(){
+    let nav = this.document.querySelector ('#nav')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+window.addEventListener("scroll", function(){
+    let nav = this.document.querySelector ('#nav-logo')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+window.addEventListener("scroll", function(){
+    let nav = this.document.querySelector ('#nav-logo-1')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+window.addEventListener("scroll", function(){
+    let nav = this.document.querySelector ('#navbar')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+window.addEventListener("scroll", function(){
+    let nav = this.document.querySelector ('#nav-btn')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
